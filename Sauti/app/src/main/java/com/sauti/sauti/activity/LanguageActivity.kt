b8d3d1dcb546fee64cc48ad1729150d038dd2e00
Patrick Martin <patrickmartin88@gmail.com>
2019-07-11 09:55:16 -0700

    Creating Language Activity

---
+++ Sauti/app/src/main/java/com/sauti/sauti/activity/LanguageActivity.kt
@@ -0,0 +1,13 @@
+package com.sauti.sauti.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sauti.sauti.R
+
+class LanguageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
