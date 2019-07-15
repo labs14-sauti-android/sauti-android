9c17917435dc0efa626fda3044dde472b2bb0a2a
Patrick Martin <patrickmartin88@gmail.com>
2019-07-15 15:34:23 -0700

    Added delay to splash_screen

--- Sauti/app/src/main/java/com/labs/sauti/activity/LanguageActivity.kt
+++ Sauti/app/src/main/java/com/labs/sauti/activity/LanguageActivity.kt
@@ -3,14 +3,21 @@ package com.labs.sauti.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.labs.sauti.R
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_language.*
+import java.util.concurrent.TimeUnit
 
-class LanguageActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+class LanguageActivity : AppCompatActivity() {
 
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,11 +39,18 @@ class LanguageActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
             finish()
         }*/
+
+        val intent = Intent(this@LanguageActivity, DashboardActivity::class.java)
+
+        compositeDisposable.add(Completable.timer(3000, TimeUnit.MILLISECONDS)
+            .subscribe {startActivity(intent)}
+        )
+
     }
 
 
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.clear()
     }
-
 }
