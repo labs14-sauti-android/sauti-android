a3ede9305722503faa2f53384399d47ba5a5ae1a
Israel dela Cruz <igdcruz42@gmail.com>
2019-07-14 20:44:52 -0500

    Added menu to nav drawer

---
+++ Sauti/app/src/main/java/com/labs/sauti/activity/DashBoardActivity.kt
@@ -0,0 +1,13 @@
+package com.labs.sauti.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.labs.sauti.R
+
+class DashBoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dash_board)
+    }
+}
