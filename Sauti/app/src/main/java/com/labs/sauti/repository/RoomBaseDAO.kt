62c057b2afdd9eee1424f424d43a237c2acc4134
Patrick Martin <patrickmartin88@gmail.com>
2019-07-15 17:59:11 -0700

    Added base Room Dao to cutback on code.

--- Sauti/app/src/main/java/com/labs/sauti/repository/RoomBaseDAO.kt
+++ Sauti/app/src/main/java/com/labs/sauti/repository/RoomBaseDAO.kt
@@ -1,2 +1,17 @@
 package com.labs.sauti.repository
 
+import androidx.room.*
+import io.reactivex.Completable
+
+
+@Dao
+interface BaseDao<in T> {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(type: T): Completable
+
+    @Delete
+    fun delete(type: T) : Completable
+
+    @Update
+    fun update(type: T) : Completable
+}
