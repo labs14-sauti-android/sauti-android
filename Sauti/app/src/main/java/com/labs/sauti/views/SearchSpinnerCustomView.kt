6210042d2bd8c1f574be46639aed3eed2293d351
Patrick Martin <patrickmartin88@gmail.com>
2019-07-29 12:46:44 -0700

    Search info custom view complete. needs logic

---
+++ Sauti/app/src/main/java/com/labs/sauti/views/SearchSpinnerCustomView.kt
@@ -0,0 +1,18 @@
+package com.labs.sauti.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.labs.sauti.R
+
+class SearchSpinnerCustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 1
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        View.inflate(getContext(), R.layout.item_search_spinner, this)
+    }
+}
