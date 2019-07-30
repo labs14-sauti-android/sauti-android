6def6caaa24362bbac6ef514c9b4c6835e4eff01
Israel dela Cruz <igdcruz42@gmail.com>
2019-07-30 11:16:51 -0500

    Dashboard navigation

--- Sauti/app/src/main/java/com/labs/sauti/fragment/DashboardFragment.kt
+++ Sauti/app/src/main/java/com/labs/sauti/fragment/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.labs.sauti.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 import com.labs.sauti.R
+import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
+
+    private var onReplaceFragmentListener: OnReplaceFragmentListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +26,61 @@ class DashboardFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        c_market_price.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(MarketPriceFragment::class.java)
+        }
+
+        c_exchange_rates.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(ExchangeRatesFragment::class.java)
+        }
+
+        c_trade_info.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(TradeInfoFragment::class.java)
+        }
+
+        c_tax_calculator.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(TaxCalculatorFragment::class.java)
+        }
+
+        t_marketplace.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(MarketplaceFragment::class.java)
+        }
+
+        t_report.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(ReportFragment::class.java)
+        }
+
+        t_help.setOnClickListener {
+            onReplaceFragmentListener?.onReplaceFragment(HelpFragment::class.java)
+        }
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() =
             DashboardFragment()
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+
+        if (context is OnReplaceFragmentListener) {
+            onReplaceFragmentListener = context
+        } else {
+            throw RuntimeException("Context must implement OnReplaceFragmentListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+        onReplaceFragmentListener = null
+    }
+
+    interface OnReplaceFragmentListener {
+        fun <T : Fragment> onReplaceFragment(c: Class<T>)
     }
 }
