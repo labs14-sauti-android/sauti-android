72c441e0c18c8a51bcebab87f6bcd67189c90670
Patrick Martin <patrickmartin88@gmail.com>
2019-07-31 14:20:08 -0700

    Created TradeInfoComponent and added to MainComponent

---
+++ Sauti/app/src/main/java/com/labs/sauti/di/component/TradeInfoComponent.kt
@@ -0,0 +1,12 @@
+package com.labs.sauti.di.component
+
+import com.labs.sauti.di.module.TradeInfoModule
+import com.labs.sauti.fragment.TradeInfoFragment
+import com.labs.sauti.fragment.TradeInfoSearchFragment
+import dagger.Subcomponent
+
+@Subcomponent(modules = [TradeInfoModule::class])
+interface TradeInfoComponent {
+    fun inject(tradeInfoFragment: TradeInfoFragment)
+    fun inject(tradeInfoSearchFragment: TradeInfoSearchFragment)
+}
