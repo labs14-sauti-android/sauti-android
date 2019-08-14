68fd71a92288a93d06a8c1a9aa343f19ea25c251
Israel dela Cruz <igdcruz42@gmail.com>
2019-08-14 15:28:48 -0500

    currency format

--- Sauti/app/src/main/java/com/labs/sauti/fragment/ExchangeRateFragment.kt
+++ Sauti/app/src/main/java/com/labs/sauti/fragment/ExchangeRateFragment.kt
@@ -21,6 +21,7 @@ import com.labs.sauti.model.exchange_rate.ExchangeRateConversionResult
 import com.labs.sauti.view_model.ExchangeRateViewModel
 import kotlinx.android.synthetic.main.fragment_exchange_rate.*
 import kotlinx.android.synthetic.main.item_recent_exchange_rate.view.*
+import java.text.DecimalFormat
 import javax.inject.Inject
 
 class ExchangeRateFragment : Fragment(), ExchangeRateConvertFragment.OnConversionCompletedListener,
@@ -96,10 +97,11 @@ OnFragmentFullScreenStateChangedListener{
     }
 
     private fun setConversionResultDetails(exchangeRateConversionResult: ExchangeRateConversionResult) {
-        val amountStr = String.format("%.2f", exchangeRateConversionResult.amount)
-        val resultStr = String.format("%.2f", exchangeRateConversionResult.result)
+        val decimalFormat = DecimalFormat("#,##0.00")
+        val amountStr = decimalFormat.format(exchangeRateConversionResult.amount)
+        val resultStr = decimalFormat.format(exchangeRateConversionResult.result)
         t_details_result.text = "$amountStr ${exchangeRateConversionResult.fromCurrency} is $resultStr ${exchangeRateConversionResult.toCurrency}"
-        val toPerFromStr = String.format("%.2f", exchangeRateConversionResult.toPerFrom)
+        val toPerFromStr = decimalFormat.format(exchangeRateConversionResult.toPerFrom)
         t_details_to_per_from.text = "(1${exchangeRateConversionResult.fromCurrency} = $toPerFromStr ${exchangeRateConversionResult.toCurrency})"
     }
 
@@ -118,10 +120,11 @@ OnFragmentFullScreenStateChangedListener{
             itemView.t_recent_todays_intl_exchange_rates.text = localeCtx.getString(R.string.today_s_intl_exchange_rates)
             itemView.t_recent_todays_intl_exchange_rates.typeface = Typeface.DEFAULT_BOLD
 
-            val amountStr = String.format("%.2f", conversionResult.amount)
-            val resultStr = String.format("%.2f", conversionResult.result)
+            val decimalFormat = DecimalFormat("#,##0.00")
+            val amountStr = decimalFormat.format(conversionResult.amount)
+            val resultStr = decimalFormat.format(conversionResult.result)
             itemView.t_recent_result.text = "$amountStr ${conversionResult.fromCurrency} is $resultStr ${conversionResult.toCurrency}"
-            val toPerFromStr = String.format("%.2f", conversionResult.toPerFrom)
+            val toPerFromStr = decimalFormat.format(conversionResult.toPerFrom)
             itemView.t_recent_to_per_from.text = "(1${conversionResult.fromCurrency} = $toPerFromStr ${conversionResult.toCurrency})"
 
             itemView.setOnClickListener {
