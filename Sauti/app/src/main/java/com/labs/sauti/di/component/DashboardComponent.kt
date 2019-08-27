e855ff1d617805478fea9c055d4bf1f513fbf081
Israel dela Cruz <igdcruz42@gmail.com>
2019-08-27 07:40:27 -0500

    dashboard recent searches displayed

--- Sauti/app/src/main/java/com/labs/sauti/di/component/DashboardComponent.kt
+++ Sauti/app/src/main/java/com/labs/sauti/di/component/DashboardComponent.kt
@@ -2,9 +2,11 @@ package com.labs.sauti.di.component
 
 import com.labs.sauti.di.module.DashboardModule
 import com.labs.sauti.fragment.DashboardFavoritesFragment
+import com.labs.sauti.fragment.DashboardRecentSearchesFragment
 import dagger.Subcomponent
 
 @Subcomponent(modules = [DashboardModule::class])
 interface DashboardComponent {
     fun inject(dashboardFavoritesFragment: DashboardFavoritesFragment)
+    fun inject(dashboardRecentSearchesFragment: DashboardRecentSearchesFragment)
 }
--- Sauti/app/src/main/java/com/labs/sauti/fragment/DashboardRecentSearchesFragment.kt
+++ Sauti/app/src/main/java/com/labs/sauti/fragment/DashboardRecentSearchesFragment.kt
@@ -6,13 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.labs.sauti.R
+import com.labs.sauti.SautiApp
+import com.labs.sauti.adapter.DashboardFavoritesAdapter
+import com.labs.sauti.view_model.DashboardRecentSearchesViewModel
+import kotlinx.android.synthetic.main.fragment_dashboard_recent_searches.*
+import javax.inject.Inject
 
 class DashboardRecentSearchesFragment : Fragment() {
 
+    @Inject
+    lateinit var dashboardRecentSearchesViewModelFactory: DashboardRecentSearchesViewModel.Factory
+
+    private lateinit var dashboardRecentSearchesViewModel: DashboardRecentSearchesViewModel
+
+    private lateinit var dashboardFavoritesAdapter: DashboardFavoritesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (context!!.applicationContext as SautiApp).dashboardComponent.inject(this)
+        dashboardRecentSearchesViewModel = ViewModelProviders
+            .of(this, dashboardRecentSearchesViewModelFactory)
+            .get(DashboardRecentSearchesViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -25,6 +45,25 @@ class DashboardRecentSearchesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        dashboardFavoritesAdapter = DashboardFavoritesAdapter(mutableListOf()) {
+            // TODO
+        }
+
+        r_recent_searches.layoutManager = LinearLayoutManager(context!!, RecyclerView.HORIZONTAL, false)
+        r_recent_searches.adapter = dashboardFavoritesAdapter
+
+        dashboardRecentSearchesViewModel.getRecentSearchesViewState().observe(this, Observer {
+            if (it.isLoading) {
+                vs_recent_searches_loading.displayedChild = 1
+            } else {
+                vs_recent_searches_loading.displayedChild = 0
+                it.recentSearches?.let {recentSearches ->
+                    dashboardFavoritesAdapter.setFavorites(recentSearches)
+                }
+            }
+        })
+        dashboardRecentSearchesViewModel.getRecentSearches()
     }
 
     companion object {
@@ -32,4 +71,6 @@ class DashboardRecentSearchesFragment : Fragment() {
         fun newInstance() =
             DashboardRecentSearchesFragment()
     }
+
+
 }
