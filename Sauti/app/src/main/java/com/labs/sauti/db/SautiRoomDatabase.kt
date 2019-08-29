735d6e0703d58609d30d5130986b6d342f8b85d4
Patrick Martin <patrickmartin88@gmail.com>
2019-08-29 03:21:20 -0700

    fixed async errors.

--- Sauti/app/src/main/java/com/labs/sauti/db/SautiRoomDatabase.kt
+++ Sauti/app/src/main/java/com/labs/sauti/db/SautiRoomDatabase.kt
@@ -14,7 +14,7 @@ import com.labs.sauti.model.market_price.FavoriteMarketPriceSearchData
 import com.labs.sauti.model.trade_info.*
 
 
-const val DATABASE_SCHEMA_VERSION = 28
+const val DATABASE_SCHEMA_VERSION = 29
 const val DB_NAME = "local-db"
 
 
